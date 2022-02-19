56695775abe4e02c3953ba06fe15a40d05bd7cd0
Urahs <sharumaku@gmail.com>
2022-02-19 21:16:50 +0300

    ...

--- LastEvent.py
+++ LastEvent.py
@@ -12,38 +12,80 @@ except:
     None
 
 
-"""
+
+
+
+key = "0"
+while True:
+    if key == "0":
+        print("""
 1 - Display
 2 - Add
 3 - Delete
 4 - Event
 5 - Exit
-"""
-
-
-key = 0
-while True:
-    if key == 0:
-        # print main menu
+""")
         key = input("What do u want to do my friend?\n>>> ")
     
-    elif key == 1:
-        # open txt read all shit then print
-        key = 0
-    elif key == 2:
+    # DISPLAY
+    if key == "1":
+        os.chdir("C:\\Users\\"+a+"\\Last Event Save File\\")
+        file = open("save.txt", "r")
+        lines = file.readlines()
+        if lines == []:
+            print("You don't have any event. Go create some...")
+            continue
+        else:
+
+
+            # find the max length of string
+            max_length = 0
+            for i in lines:
+                temp_len = len(i.split(" ")[0])
+                if temp_len > max_length:
+                    max_length = temp_len
+            print("\n------ Your Events ------")
+            for entity in lines:
+    
+                event = entity.split(" ")
+                event_name = event[0]
+                #print event name1
+
+                print(event_name, end="")
+                for i in range(0, max_length - len(event_name)):
+                    print(" ", end="")
+                
+                
+                #print last time
+                
+
+                print()
+            print("-------------------------")
+        key = "0"
+
+    # ADD
+    elif key == "2":
         add_event = input("Please enter a new event\n>>> ")
         # open txt file with append, then enter add new event like this (event_name 1000 24:00)
-        key = 0
-    elif key == 3:
+        key = "0"
+
+    # DELETE
+    elif key == "3":
         delete_event = input("Please enter the name of event you wanna delete\n>>> ")
         #open txt read all shit, find that then delete it. Finally write all shit again
-        key = 0
-    elif key == 4:
+        key = "0"
+
+    # EVENT
+    elif key == "4":
         # read and print all shit
         event = input("Please select the event\n>>> ")
         # find that shit and update
-        key = 0
-    elif key == 5:
+        key = "0"
+
+    # EXIT
+    elif key == "5":
         exit()
-    elif key == 6:
+
+    # SECRET ONE
+    elif key == "6":
         print("This app is made by Selim Sarialtin, you don't want to stole it...")
